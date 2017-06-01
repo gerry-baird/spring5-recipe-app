76ad7b1eb0000e5cc8571e4781c7c133565ec594
John Thompson <john@springframework.guru>
2017-06-01 17:59:57 -0400

    adding index Page

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -3,13 +3,14 @@ package guru.springframework.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Created by jt on 6/1/17.
+ */
 @Controller
 public class IndexController {
 
-    @RequestMapping({"","/","index"})
+    @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
-
-        System.out.println("Ugg");
         return "index";
     }
 }
