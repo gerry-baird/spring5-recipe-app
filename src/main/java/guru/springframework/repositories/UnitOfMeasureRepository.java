ffe48ea4dca0f6c6fcb686c807bb51d2bf1a4145
gerry <gerry.baird@uk.ibm.com>
2021-06-04 14:43:49 +0100

    Some aditional comment

--- src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
+++ src/main/java/guru/springframework/repositories/UnitOfMeasureRepository.java
@@ -11,6 +11,7 @@ import java.util.Optional;
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
 
     //nice and helpful
+    //Added again
     Optional<UnitOfMeasure> findByDescription(String description);
 
 }
